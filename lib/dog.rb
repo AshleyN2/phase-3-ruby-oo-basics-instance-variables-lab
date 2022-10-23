3464db63a94743991a26a3b948183830d5450501
Ashley <ashleynjeri7@gmail.com>
2022-10-23 17:17:08 +0300

    passed tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
+
+    # takes in an argument of a dog's name 
+    #and assigns that value to a variable
     def name=(dog_name)
-        this_dogs_name = dog_name
-      end
+        @this_dogs_name = dog_name
+    end
     
-      def name
-        this_dogs_name
-      end
+    # responsible for reporting the name
+    def name
+      @this_dogs_name
+    end
 end
